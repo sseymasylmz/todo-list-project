ddc0b780d65c311dd5e665407c26cab7b2ec03da
Şeyma Söylemez <seymasoylemez12@gmail.com>
2025-03-29 04:43:46 +0300

    Net 8.0 uyumlu güncel versiyon

--- Todo/Models/TodoItem.cs
+++ Todo/Models/TodoItem.cs
@@ -5,9 +5,15 @@ namespace Todo.Models
     public class TodoItem
     {
         public int Id { get; set; }
+
         public string Title { get; set; }
+
         public string Description { get; set; }
+
         public bool IsCompleted { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime CreatedAt { get; set; }
+
+        public string Name { get; set; }
     }
 }
